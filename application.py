394951b7ceffca6877eb06a5f6efe67c9d47072e
karan rayamajhi <karanraipur6@gmail.com>
2024-08-10 11:33:31 +0530

    first commit

--- application.py
+++ application.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import pickle
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
 
 ## import ridge regresor model and standard scaler pickle 
 ridge_model = pickle.load(open('models/ridge.pkl' , 'rb'))
@@ -13,7 +14,7 @@ standard_scaler = pickle.load(open('models/scaler.pkl' , 'rb'))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') 
 
 @app.route('/predictdata' , methods =['GET' , 'POST'])
 def predict_datapoint():
